84cf33adcd27f2aa02176679c41a16c71bc5fe39
mdraselhosen345 <rashel099009@gmail.com>
2026-02-08 03:26:16 -0500

    index

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { RouterProvider } from 'react-router'
 import { router } from './router/router.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router}></RouterProvider>
